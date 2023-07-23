a5d5ee104cb21db30f495bfdf70177c84f195ed6
aisu-programming <aisu.programming@gmail.com>
2023-07-23 15:12:32 +0800

    Two updates: 1. Add utime bounding function 2. Change sleep time from 1sec/50data to 1sec/50post

--- src/App.tsx
+++ src/App.tsx
@@ -8,37 +8,44 @@ function sleep(ms: number) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function createVideoDecoder(car: string, camera: string): VideoDecoder {
+function createVideoDecoder(car: string, camera: string, utime_start: number, utime_end: number): VideoDecoder {
     return new VideoDecoder({
         output: (videoFrame: VideoFrame) => {
-            // console.log(videoFrame);
-            const canvas = document.createElement('canvas');
-            const ctx = canvas.getContext('2d');
-            if (ctx) {
-                canvas.width = videoFrame.displayWidth;
-                canvas.height = videoFrame.displayHeight;
-                // 將 VideoFrame 的像素數據繪製到 canvas 上
-                ctx.drawImage(videoFrame, 0, 0);
-                // 使用 Fetch API 將 base64Image 傳送到後端伺服器
-                fetch("http://127.0.0.1:5000/upload", {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify({
-                        image: canvas.toDataURL("image/jpeg"),
-                        car: car,
-                        camera: camera,
-                        utime: videoFrame.timestamp,
-                    }),
-                })
-                    .then((response) => response.json())
-                    .then((data) => {
-                        // console.log(data.message);
+
+            // Filter and send only the images we want to the Back-End
+            const utime = videoFrame.timestamp;
+            if (utime_start <= utime && utime <= utime_end) {
+                const canvas = document.createElement('canvas');
+                const ctx = canvas.getContext('2d');
+
+                if (ctx) {
+                    canvas.width = videoFrame.displayWidth;
+                    canvas.height = videoFrame.displayHeight;
+
+                    // 將 VideoFrame 的像素數據繪製到 canvas 上
+                    ctx.drawImage(videoFrame, 0, 0);
+
+                    // 使用 Fetch API 將 base64Image 傳送到後端伺服器
+                    fetch("http://127.0.0.1:5000/upload", {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify({
+                            image: canvas.toDataURL("image/jpeg"),
+                            car: car,
+                            camera: camera,
+                            utime: utime,
+                        }),
                     })
-                    .catch((error) => {
-                        console.warn("圖片上傳失敗：", error);
-                    });
+                        .then((response) => response.json())
+                        .then((data) => {
+                            // console.log(data.message);
+                        })
+                        .catch((error) => {
+                            console.warn("圖片上傳失敗：", error);
+                        });
+                }
             }
             videoFrame.close();
         },
@@ -70,19 +77,9 @@ function decodeImage(decoder: VideoDecoder, image: May.image_t) {
     }
 }
 
-async function main() {
+async function main(jsonDataPathList: Array<any>, utime_start: number, utime_end: number) {
     const cameraToVideoDecoder: Record<string, any> = {};
-    const jsonDataPathList = [
-        "./sample_data/metheven/0_decoded_video.json",
-        "./sample_data/metheven/10000_decoded_video.json",
-        "./sample_data/metheven/20000_decoded_video.json",
-        "./sample_data/metheven/30000_decoded_video.json",
-        "./sample_data/metheven/40000_decoded_video.json",
-        "./sample_data/metheven/50000_decoded_video.json",
-        "./sample_data/metheven/60000_decoded_video.json",
-        "./sample_data/metheven/70000_decoded_video.json",
-        "./sample_data/metheven/80000_decoded_video.json",
-    ]
+    let post_counter = 0;
 
     for (let jsonDataPathIndex = 0; jsonDataPathIndex < jsonDataPathList.length; jsonDataPathIndex++) {
         let jsonDataPath = jsonDataPathList[jsonDataPathIndex];
@@ -98,7 +95,7 @@ async function main() {
 
                 // Create new VideoDecoder for new car-camera
                 if (!(jsonData.camera in cameraToVideoDecoder)) {
-                    var newVideoDecoder = createVideoDecoder("metheven", jsonData.camera);
+                    var newVideoDecoder = createVideoDecoder("metheven", jsonData.camera, utime_start, utime_end);
                     newVideoDecoder = configureDecoder(newVideoDecoder, {
                         codec: "vp09.00.10.08",
                         optimizeForLatency: true,
@@ -112,10 +109,15 @@ async function main() {
                 Object.assign(image, jsonData);
                 decodeImage(decoder, image);
 
-                if ((jsonDataIndex+1) % 50 == 0) {
-                    // On my computer: approximately cost 1 seconds for Back-End to process 50 pakages
-                    console.log(jsonDataPathIndex, jsonDataPath, jsonDataIndex, "Sleep for 1 seconds!");
-                    await sleep(1000);
+                const utime = Number(image.utime);
+                if (utime_start <= utime && utime <= utime_end) {
+                    post_counter++;
+                    if (post_counter >= 50) {
+                        // On my computer: approximately cost 1 seconds for Back-End to process 50 pakages
+                        console.log(jsonDataPathIndex, jsonDataPath, jsonDataIndex, "Sleep for 1 seconds!");
+                        await sleep(1000);
+                        post_counter = 0;
+                    }
                 }
             }
         }
@@ -123,7 +125,21 @@ async function main() {
     }
 }
 
-main();
+const jsonDataPathList = [
+    "./sample_data/metheven/0_decoded_video.json",
+    "./sample_data/metheven/10000_decoded_video.json",
+    "./sample_data/metheven/20000_decoded_video.json",
+    "./sample_data/metheven/30000_decoded_video.json",
+    "./sample_data/metheven/40000_decoded_video.json",
+    "./sample_data/metheven/50000_decoded_video.json",
+    "./sample_data/metheven/60000_decoded_video.json",
+    "./sample_data/metheven/70000_decoded_video.json",
+    "./sample_data/metheven/80000_decoded_video.json",
+]
+const utime_start = 1689704191610548;
+const utime_end = 1689704201809922;
+console.log("Start decoding...");
+main(jsonDataPathList, utime_start, utime_end);
 
 
 
