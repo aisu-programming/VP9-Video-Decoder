fa32907ef0566bc17e0145e311b327474e75736d
aisu-programming <aisu.programming@gmail.com>
2023-08-03 18:29:30 +0800

    Adjust preprocessing program

--- 2_convert_groups_txt.py
+++ 2_convert_groups_txt.py
@@ -1,12 +1,11 @@
-DATES = [ "_2023_08_02" ]
+DATE = "_2023_08_02"
 
 
 
-for date in DATES:
-    with open(f"src/encoded_data/{date}/{date}_groups.txt", 'r') as txt_file:
-        content = txt_file.read()
-        content = content.split("EST_tp: ")[1]
-        with open(f"src/encoded_data/{date}/{date}_groups.py", 'w') as py_file:
-            py_file.write("import datetime\n\n")
-            py_file.write("utime_interval_list = ")
-            py_file.write(content)
+with open(f"src/encoded_data/{DATE}/{DATE}_groups.txt", 'r') as txt_file:
+    content = txt_file.read()
+    content = content.split("EST_tp: ")[1]
+    with open(f"src/encoded_data/{DATE}/{DATE}_groups.py", 'w') as py_file:
+        py_file.write("import datetime\n\n")
+        py_file.write("utime_interval_list = ")
+        py_file.write(content)
